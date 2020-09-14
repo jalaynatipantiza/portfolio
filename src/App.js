d02710a586368f57c40170cb9a7234ea5938ae48
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-09-14 11:00:42 -0600

    centered projects

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Navbar from './components/nav/navbar';
 import PhotoIntro from './components/profile/landingpage';
 import ProjectCard from './components/projects/projects';
 import projects from './mockDatabase'
+import useStyles from "./appStyles"
 
 function App() {
-  console.log(projects);
+  const classes = useStyles();
   const projectList = projects.map(project => {
     return (
       <ProjectCard title={project.title} description={project.description} frontEnd={project.frontEnd} backEnd={project.backEnd} img={project.img} />
@@ -16,8 +17,10 @@ function App() {
   return (
     <div style={{backgroundColor: '#14141f'}}>
       <PhotoIntro/>
+      <div className={classes.projects}>
       <h1 style={{color: "white"}}>Latest Projects!</h1>
       {projectList}
+      </div>
     </div>
  
   );
