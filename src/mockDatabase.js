13d86326980f80d8b265b4f8f3e5dca16812043b
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-09-14 10:47:18 -0600

    add pictures for all projects

--- src/mockDatabase.js
+++ src/mockDatabase.js
@@ -1,34 +1,43 @@
+import atlasJams from './documents/AtlasJams.png'
+import brewMaps from './documents/brewMaps.png'
+import scheduler from './documents/Scheduler.png'
+import tweeter from './documents/Tweeter.png'
+import tinyApp from './documents/TinyApp.png'
 const projects = [
   {
     title: "Atlas Jams | August 2020",
     description: "A platform  for hosts, performers, and ones who are looking for music events to attend. Allows performers to advertise musical talent and hosts to showcase spaces. Allows the ability to save upcoming events in a calendar. ",
     frontEnd: "React",
     backEnd: "PSQL, Ruby on Rails",
-    img: "https://images.unsplash.com/photo-1553290322-0440fe3b1ddd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+    img: atlasJams
   },
   {
     title: "BrewMaps | July 2020",
     description: "Simple web app to allow users to view, create, edit, delete and save their favourite places, as pins, on multiple maps. A project with a timeframe of six days.",
     frontEnd: "JavaScript, HTML, CSS, Bootstrap",
-    backEnd: "SQL, AJAX(AJAJ), jQuery, Google API integration"
+    backEnd: "SQL, AJAX(AJAJ), jQuery, Google API integration",
+    img: brewMaps
   },
   {
     title: "Interview Scheduler | July 2020",
     description: "A scheduler that users can create, edit and delete interview appointments. An app that was built within a five day span and testing included.",
     frontEnd: "HTML, CSS and React, StoryBook, Cypress",
-    backEnd: "Node, Express, SQL, Jest"
+    backEnd: "Node, Express, SQL, Jest",
+    img: scheduler
   },
   {
     title: "Tweeter | June 2020",
     description: "Tweeter is a simple, single-page Twitter clone. It has a response of design targeted for mobile and desktop users",
     frontEnd: "HTML, CSS, JS",
-    backEnd: "jQuery, AJAX, Node and Express"
+    backEnd: "jQuery, AJAX, Node and Express",
+    img: tweeter
   },
    {
     title: "Tiny App | June 2020",
     description: "TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs.",
     frontEnd: "HTML, CSS, Bootstrap",
-    backEnd: "Node and Express"
+    backEnd: "Node and Express",
+    img: tinyApp
    }
 ]
 
