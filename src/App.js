fd83eb58506197cb59f04082239ab6e56f9f3340
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-09-15 14:38:34 -0600

    adding routes for root and /about

--- src/App.js
+++ src/App.js
@@ -7,25 +7,33 @@ import useStyles from "./appStyles"
 import BottomNav from './components/breadcrumbs/bottomNav';
 import Navbar from './components/nav/navbar';
 import About from './components/aboutMe/about';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 function App() {
   const classes = useStyles();
+
   const projectList = projects.map(project => {
     return (
-      <ProjectCard title={project.title} description={project.description} frontEnd={project.frontEnd} backEnd={project.backEnd} img={project.img} link={project.link} />
+        <ProjectCard title={project.title} description={project.description} frontEnd={project.frontEnd} backEnd={project.backEnd} img={project.img} link={project.link} />
     )
   })
   return (
-    <div style={{backgroundColor: '#14141f'}}>
-      <Navbar/>
-      {/* <PhotoIntro/> */}
-      <About/>
-      <div className={classes.projects}>
-      <h1 style={{color: "white", fontFamily: "initial", fontSize: "x-large"}}>CHECK OUT MY WORK!</h1>
-      {projectList}
-      </div>
-      <BottomNav/> 
-    </div>
+    <Router>
+      <main>
+        <section>
+          <div style={{backgroundColor: '#14141f'}}>
+            <Route exactl={true} path='/' component={Navbar} />
+            <Route exact={true} path='/about' component={About} />
+            <Route exactl={true} path='/' component={PhotoIntro} />
+            <div className={classes.projects}>
+              <h1 style={{color: "white", fontFamily: "initial", fontSize: "x-large"}}>CHECK OUT MY WORK!</h1>
+              <Route exactl={true} path='/' render={() => projectList} />
+            </div>
+            <Route exactl={true} path='/' component={BottomNav}/>
+          </div>
+        </section>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/components/nav/menuButton.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Fade from '@material-ui/core/Fade';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+export default function FadeMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    
+  };
+
+  return (
+    <div>
+      <IconButton edge="start" color="inherit" onClick={handleClick} aria-controls="fade-menu" aria-haspopup="true" >
+         <MenuIcon />
+      </IconButton>
+      <Menu
+        id="fade-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+        >
+        <MenuItem onClick={handleClose}>About</MenuItem>
+        <MenuItem onClick={handleClose}>Contact Me</MenuItem>
+        <MenuItem onClick={handleClose}>Blog</MenuItem>
+      </Menu>
+
+    </div>
+  );
+}
