68e4cf595ecd2e352fbf627b900c319b0324b23a
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-09-16 11:50:10 -0600

    playing around with switch and deleted unwanted home file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import './App.css';
-// import useStyles from "./appStyles"
+import useStyles from "./appStyles"
 import About from './components/aboutMe/about';
-import { BrowserRouter as Router,  Route } from 'react-router-dom';
-import Home from './components/home';
+import { BrowserRouter as Router,  Route, Switch } from 'react-router-dom';
+import ProjectCard from './components/projects/projects';
+import projects from './mockDatabase'
+import PhotoIntro from './components/profile/landingpage';
+import Navbar from './components/nav/navbar';
+import BottomNav from './components/breadcrumbs/bottomNav'
 
 
 function App() {
-  // const classes = useStyles();
-
+  const classes = useStyles();
+  const projectList = projects.map(project => {
+    return (
+        <ProjectCard title={project.title} description={project.description} frontEnd={project.frontEnd} backEnd={project.backEnd} img={project.img} link={project.link} />
+    )
+  })
   return (
     <Router>
       <main>
         <section>
-          
-            <Route exact={true} path='/about' component={About} />
-            <Route path='/' component={Home}/>
-          
+        <div style={{backgroundColor: '#14141f'}}>
+        <Route  path='/' component={Navbar} />
+        <Switch>
+          <Route exact={true} path='/' component={PhotoIntro} />
+          <Route exact={true} path='/' component={About}/>
+        </Switch>
+        <div className={classes.projects}>
+          <h1 style={{color: "white", fontFamily: "initial", fontSize: "x-large"}}>CHECK OUT MY WORK!</h1>
+          <Route path='/' render={() => projectList} />
+        </div>
+        <Route path='/' component={BottomNav}/>
+          </div>
         </section>
       </main>
     </Router>
--- src/components/aboutMe/about.js
+++ src/components/aboutMe/about.js
@@ -2,47 +2,27 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import useStyles from "./aboutStyles"
 import img from '../../documents/jalayna.jpeg'
-import Switch from '@material-ui/core/Switch';
 import Paper from '@material-ui/core/Paper';
-import Slide from '@material-ui/core/Slide';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import DescriptionBox from './descriptionBox';
 import TechStackBox from './techStackBox';
 
 
 export default function About() {
   const classes = useStyles();
-  const [checked, setChecked] = React.useState(false);
-  const handleChange = () => {
-    setChecked((prev) => !prev);
-  };
-
+  
   return (
   <React.Fragment>
   <CssBaseline />
     <div className={classes.aboutProfileImg}>
       <img className={classes.profilePicture} src={img} alt="profilePic"/>
-      
         <div className={classes.root}>
-          <div className={classes.wrapper}>
-            <FormControlLabel
-              control={<Switch checked={checked} onChange={handleChange} />}
-              label="Show"
-            />
-            <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
-              <Paper elevation={4} className={classes.paper}>
-                <DescriptionBox/>
-              </Paper>
-            </Slide>
-            <Slide direction="up" in={checked} mountOnEnter unmountOnExit>
-              <Paper elevation={4} className={classes.paper}>
-              <TechStackBox/>
-              </Paper>
-            </Slide>
-            
-          </div>
+          <Paper elevation={4} className={classes.paper}>
+            <DescriptionBox/>
+          </Paper>
+          <Paper elevation={4} className={classes.paper}>
+          <TechStackBox/>
+          </Paper>
         </div>
-
     </div>
   </React.Fragment> 
   );
