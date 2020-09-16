ed9a2dc7589693e222fe4c7103fe4f975600a690
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-09-16 16:44:03 -0600

    change nav bar to non-menu item

--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useStyles from './styles';
-import FadeMenu from './menuButton';
-
+import { useHistory } from "react-router-dom";
+import HomeIcon from '@material-ui/icons/Home';
 
 export default function Navbar() {
   const classes = useStyles();
 
+  const history = useHistory();
+
+  const handleAboutClick = () => {
+    history.push("/about")
+  }
+  const handleHomeClick = () => {
+    history.push("/")
+  }
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
         <Toolbar className={classes.toolBar}>
-            <FadeMenu/>
+          <HomeIcon className={classes.homeButton} onClick={handleHomeClick} />
+          <div className={classes.endButtons}>
+            <button className={classes.buttons} onClick={handleAboutClick}>About</button>
+            <button className={classes.buttons} onClick={handleAboutClick}>Contact Me</button>
+            <button className={classes.buttons} onClick={handleAboutClick}>Blog</button>   
+          </div>
         </Toolbar>
       </AppBar>
     </div>
--- src/components/nav/styles.js
+++ src/components/nav/styles.js
@@ -6,13 +6,30 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
 
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   appBar: {
       position: "fixed",
       backgroundColor: "transparent",
+      boxShadow: "none",
+  },
+  buttons: {
+    color: "white",
+    backgroundColor: "transparent",
+    border: "none",
+    outline: "none",
+    fontSize: "20px",
+    padding: "0 50px"
+  },
+ 
+  toolBar: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  endButtons: {
+    display: "flex",
+    justifyContent: "flex-end",
   }
+
+ 
 }));
 
 export default useStyles;
