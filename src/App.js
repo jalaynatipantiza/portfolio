443f5ee9cacc3c59706061c8317e4e8cae6a7c59
Jalayna Tipantiza <jalaynatipantiza@jalaynas-mbp.gateway.sasktel.net>
2020-10-18 11:53:18 -0600

    add transition back in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import useStyles from "./appStyles"
 import About from './components/aboutMe/about';
 import { BrowserRouter as Router,  Route, Switch } from 'react-router-dom';
 import PhotoIntro from './components/profile/landingpage';
 import Navbar from './components/nav/navbar';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
 import Slide from 'react-reveal/Slide';
 import ProList from '../src/components/projects/proList';
 
@@ -13,6 +14,13 @@ function App() {
   const classes = useStyles();
  
   const [offsetY, setOffsetY] = useState(0)
+  // const handleScroll = () => setOffsetY(window.pageYOffset)
+
+  // useEffect(() =>{
+  //   window.addEventListener("scroll", handleScroll)
+  //   return () => window.removeEventListener("scroll", handleScroll);
+
+  // }, [])
 
   return (
     <Router>
@@ -21,12 +29,19 @@ function App() {
         <div className={classes.background1} style={{transform: `translateY(-${offsetY * 0.5}px)`}}>
         <Navbar/>
         <Route render={({location}) => (
-       
+          <TransitionGroup>
+            <CSSTransition
+              key={location.key}
+              timeout={450}
+              classNames="fade"
+            >
               <Switch location={location}>
                 <Slide left>
                 <Route  exact path="/" component={PhotoIntro} />
                 </Slide>
               </Switch>
+            </CSSTransition>
+           </TransitionGroup>
         )} />
         <Route path="/about" component={About} />
         <Route path="/projects" component={ProList}/>
